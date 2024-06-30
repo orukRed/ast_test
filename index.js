f22462208498d4ce29103dc97dce2922e47081cf
orukRed <orukRed@gmail.com>
2024-06-30 23:48:14 +0900

    add

--- index.js
+++ index.js
@@ -46,8 +46,10 @@ const ast = babel.parse(`
     allowSuperOutsideMethod: true,
 });
 const variablePrefixList = ["f", "sf", "tf", "mp"];
+let hoge = "";
 (0, traverse_1.default)(ast, {
     enter: (path) => {
+        // console.log(JSON.stringify(path.node, null, 2))
     },
     Identifier(path) {
         // console.log('Identifier', path.node.name);
@@ -58,11 +60,31 @@ const variablePrefixList = ["f", "sf", "tf", "mp"];
             if (left.property.type === 'Identifier') {
                 // 'f.' をキー名の先頭に追加してプロパティ名を取得
                 const propertyName = 'f.' + left.property.name;
-                console.log("MemberExpression", propertyName); // ここで取得したプロパティ名を使用
+                // console.log("MemberExpression", propertyName); // ここで取得したプロパティ名を使用
                 objects.push(propertyName);
+                // console.log(JSON.stringify(path.node, null, 2))
+                // hoge = propertyName;
             }
         }
-    }
+    },
+    ObjectExpression: (path) => {
+        const lastIndex = objects.length - 1;
+        // console.log(JSON.stringify(path.node, null, 2))
+        path.node.properties.forEach(property => {
+            // SpreadElementには`key`プロパティがないため、ObjectMethodまたはObjectPropertyであることを確認します
+            if (property.type === 'ObjectMethod' || property.type === 'ObjectProperty') {
+                console.log(path.node);
+                console.log(property.key);
+                if (property.key.type === 'Identifier') {
+                    objects.push(property.key.name);
+                }
+            }
+            else {
+                // SpreadElementの場合の処理
+                console.log('SpreadElementはkeyプロパティを持ちません。');
+            }
+        });
+    },
 });
 // console.log("astを出力するよ")
 // console.log(JSON.stringify(ast, null, 2));
--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import { SpreadElement } from '@babel/types';
 
 
 
-const objects: any = [];
+const objects: any[] = [];
 const ast = babel.parse(
   `
   f.tmp_index = "message";
@@ -28,9 +28,10 @@ const ast = babel.parse(
 });
 
 const variablePrefixList = ["f", "sf", "tf", "mp"];
+let hoge = "";
 traverse(ast, {
   enter: (path) => {
-
+    // console.log(JSON.stringify(path.node, null, 2))
   },
   Identifier(path) {
     // console.log('Identifier', path.node.name);
@@ -41,11 +42,31 @@ traverse(ast, {
       if (left.property.type === 'Identifier') {
         // 'f.' をキー名の先頭に追加してプロパティ名を取得
         const propertyName = 'f.' + left.property.name;
-        console.log("MemberExpression", propertyName); // ここで取得したプロパティ名を使用
+        // console.log("MemberExpression", propertyName); // ここで取得したプロパティ名を使用
         objects.push(propertyName);
+        // console.log(JSON.stringify(path.node, null, 2))
+        // hoge = propertyName;
       }
     }
-  }
+  },
+  ObjectExpression: (path) => {
+    const lastIndex = objects.length - 1;
+
+    // console.log(JSON.stringify(path.node, null, 2))
+    path.node.properties.forEach(property => {
+      // SpreadElementには`key`プロパティがないため、ObjectMethodまたはObjectPropertyであることを確認します
+      if (property.type === 'ObjectMethod' || property.type === 'ObjectProperty') {
+        console.log(path.node)
+        console.log(property.key);
+        if (property.key.type === 'Identifier') {
+          objects.push(property.key.name);//ここでネストしたオブジェクトをプッシュしている
+        }
+      } else {
+        // SpreadElementの場合の処理
+        console.log('SpreadElementはkeyプロパティを持ちません。');
+      }
+    });
+  },
 });
 // console.log("astを出力するよ")
 // console.log(JSON.stringify(ast, null, 2));
