1c59b4991a92c8cb647c286116841dbc8a01e609
orukRed <orukRed@gmail.com>
2024-07-01 21:37:57 +0900

    ネストしたオブジェクトの構文解析うまくいった……？

--- index.js
+++ index.js
@@ -28,6 +28,41 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const babel = __importStar(require("@babel/parser"));
 const traverse_1 = __importDefault(require("@babel/traverse"));
+class VariableData {
+    constructor(_name, _value, type, description = "", nestVariableData = []) {
+        this._nestVariableData = []; //ネストされた変数情報を格納するための配列//FIXME:まだ未使用 将来的にネスト↓オブジェクトのために使いたい
+        this.location = "";
+        this._name = _name;
+        this._value = _value;
+        this._type = type;
+        this._description = description;
+        this._nestVariableData = nestVariableData;
+    }
+}
+function getNestedObject(property, parentName = '') {
+    let nestedObjects = [];
+    if (property.type === 'ObjectExpression') {
+        property.properties.forEach((prop) => {
+            if (prop.key && prop.key.type === 'Identifier') {
+                const name = parentName ? `${parentName}.${prop.key.name}` : prop.key.name;
+                const value = prop.value.type === 'ObjectExpression' ? undefined : prop.value.value;
+                const variableData = new VariableData(name, value, undefined);
+                if (prop.value.type === 'ObjectExpression') {
+                    variableData._nestVariableData = getNestedObject(prop.value, name);
+                }
+                nestedObjects.push(variableData);
+            }
+        });
+    }
+    return nestedObjects;
+}
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+const temp = [];
 const objects = [];
 const ast = babel.parse(`
   f.tmp_index = "message";
@@ -37,7 +72,9 @@ const ast = babel.parse(`
   f.hoge = {
     foo:{
         bar:1
-    }
+    },
+    piyo:[1,2,3],
+    fufa:["a","b","c"]
   };  
 `, {
     allowAwaitOutsideFunction: true,
@@ -61,7 +98,8 @@ let hoge = "";
                 // 'f.' をキー名の先頭に追加してプロパティ名を取得
                 const propertyName = 'f.' + left.property.name;
                 // console.log("MemberExpression", propertyName); // ここで取得したプロパティ名を使用
-                objects.push(propertyName);
+                const variableData = new VariableData(propertyName, undefined, "f");
+                objects.push(variableData);
                 // console.log(JSON.stringify(path.node, null, 2))
                 // hoge = propertyName;
             }
@@ -69,14 +107,26 @@ let hoge = "";
     },
     ObjectExpression: (path) => {
         const lastIndex = objects.length - 1;
+        const lastObject = objects[lastIndex];
         // console.log(JSON.stringify(path.node, null, 2))
         path.node.properties.forEach(property => {
             // SpreadElementには`key`プロパティがないため、ObjectMethodまたはObjectPropertyであることを確認します
             if (property.type === 'ObjectMethod' || property.type === 'ObjectProperty') {
-                console.log(path.node);
-                console.log(property.key);
+                // console.log(path.node)
+                // console.log(property.key);
                 if (property.key.type === 'Identifier') {
-                    objects.push(property.key.name);
+                    const nestedObjects = getNestedObject(path.node);
+                    if (lastObject._nestVariableData.length <= 0) {
+                        // objects.pop();
+                        lastObject._nestVariableData = nestedObjects;
+                        // objects.push(lastObject);//ここでネストしたオブジェクトをプッシュしている クラスに入れるとしたら、ここで再帰処理とか使ってnestObjectに入れる感じになるのかなあ？
+                        console.log("--------------------");
+                    }
+                    // const hoge = getNestedObject(property);
+                    // console.log(hoge)
+                    // temp.push(hoge);
+                    // // const nestObjectList = getNestObject(property);
+                    // // lastObject._nestVariableData = nestObjectList;
                 }
             }
             else {
@@ -91,6 +141,7 @@ let hoge = "";
 // console.log("objectsを出力するよ")
 // console.log(JSON.stringify(objects, null, 2))
 console.log(objects);
+// console.log(temp)
 console.log("終了するよ");
 process.exit();
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -5,10 +5,55 @@ import { Expression } from '@babel/types';
 import { ObjectMethod } from '@babel/types';
 import { ObjectProperty } from '@babel/types';
 import { SpreadElement } from '@babel/types';
+import { get } from 'http';
+
+class VariableData {
+  public _name: string;//変数名
+  public _value: string | undefined;//変数の値 現在未使用だけど今後使うかもなので一応定義だけしておく
+  public _description: string | undefined;//変数の説明
+  public _nestVariableData: VariableData[] = [];//ネストされた変数情報を格納するための配列//FIXME:まだ未使用 将来的にネスト↓オブジェクトのために使いたい
+  public _type: string | undefined;//変数の種類 f sf tf mpのいずれか
+  public location: string = "";
+
+  public constructor(_name: string, _value: string | undefined, type: string | undefined, description: string = "", nestVariableData: VariableData[] = []) {
+    this._name = _name;
+    this._value = _value;
+    this._type = type;
+    this._description = description;
+    this._nestVariableData = nestVariableData;
+  }
+}
 
 
+function getNestedObject(property: any, parentName: string = ''): VariableData[] {
+  let nestedObjects: VariableData[] = [];
+  if (property.type === 'ObjectExpression') {
+    property.properties.forEach((prop: any) => {
+      if (prop.key && prop.key.type === 'Identifier') {
+        const name = parentName ? `${parentName}.${prop.key.name}` : prop.key.name;
+        const value = prop.value.type === 'ObjectExpression' ? undefined : prop.value.value;
+        const variableData = new VariableData(name, value, undefined);
+        if (prop.value.type === 'ObjectExpression') {
+          variableData._nestVariableData = getNestedObject(prop.value, name);
+        }
+        nestedObjects.push(variableData);
+      }
+    });
+  }
+  return nestedObjects;
+}
 
-const objects: any[] = [];
+
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------
+
+
+const temp: any[] = []
+const objects: VariableData[] = [];
 const ast = babel.parse(
   `
   f.tmp_index = "message";
@@ -18,7 +63,9 @@ const ast = babel.parse(
   f.hoge = {
     foo:{
         bar:1
-    }
+    },
+    piyo:[1,2,3],
+    fufa:["a","b","c"]
   };  
 `, {
   allowAwaitOutsideFunction: true,
@@ -26,6 +73,7 @@ const ast = babel.parse(
   errorRecovery: true,
   allowSuperOutsideMethod: true,
 });
+
 
 const variablePrefixList = ["f", "sf", "tf", "mp"];
 let hoge = "";
@@ -43,7 +91,8 @@ traverse(ast, {
         // 'f.' をキー名の先頭に追加してプロパティ名を取得
         const propertyName = 'f.' + left.property.name;
         // console.log("MemberExpression", propertyName); // ここで取得したプロパティ名を使用
-        objects.push(propertyName);
+        const variableData = new VariableData(propertyName, undefined, "f");
+        objects.push(variableData);
         // console.log(JSON.stringify(path.node, null, 2))
         // hoge = propertyName;
       }
@@ -51,15 +100,27 @@ traverse(ast, {
   },
   ObjectExpression: (path) => {
     const lastIndex = objects.length - 1;
-
+    const lastObject = objects[lastIndex];
     // console.log(JSON.stringify(path.node, null, 2))
     path.node.properties.forEach(property => {
       // SpreadElementには`key`プロパティがないため、ObjectMethodまたはObjectPropertyであることを確認します
       if (property.type === 'ObjectMethod' || property.type === 'ObjectProperty') {
-        console.log(path.node)
-        console.log(property.key);
+        // console.log(path.node)
+        // console.log(property.key);
         if (property.key.type === 'Identifier') {
-          objects.push(property.key.name);//ここでネストしたオブジェクトをプッシュしている
+          const nestedObjects: VariableData[] = getNestedObject(path.node);
+
+          if (lastObject._nestVariableData.length <= 0) {
+            // objects.pop();
+            lastObject._nestVariableData = nestedObjects;
+            // objects.push(lastObject);//ここでネストしたオブジェクトをプッシュしている クラスに入れるとしたら、ここで再帰処理とか使ってnestObjectに入れる感じになるのかなあ？
+            console.log("--------------------")
+          }
+          // const hoge = getNestedObject(property);
+          // console.log(hoge)
+          // temp.push(hoge);
+          // // const nestObjectList = getNestObject(property);
+          // // lastObject._nestVariableData = nestObjectList;
         }
       } else {
         // SpreadElementの場合の処理
@@ -73,5 +134,6 @@ traverse(ast, {
 // console.log("objectsを出力するよ")
 // console.log(JSON.stringify(objects, null, 2))
 console.log(objects)
+// console.log(temp)
 console.log("終了するよ")
 process.exit();
